0542b393195893955daed2bf506a10eee6aaa641
Travis Gooden <tgooden@my.bcit.ca>
2024-03-23 12:13:45 -0700

    Update API key code

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 
+const API = process.env.API;
 app.use(cors());
 
 app.listen(5500, () => {
@@ -12,7 +13,7 @@ app.listen(5500, () => {
 app.get("/getWeatherByCity", (req, res) => {
   x = undefined;
   fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${req.query.city}&appid={process.env.API}&units=metric`,
+    `https://api.openweathermap.org/data/2.5/weather?q=${req.query.city}&appid={API}&units=metric`,
   )
     .then((resp) => resp.json())
     .then((resp) => {
